88c12cd2368cdb8f8ae1f5f9d7449ffba6db533e
Jakub Lemka <jakub.lemka@lppsa.com>
2020-04-05 10:13:09 +0200

    /nwd endpoint exercise

--- multicontapp/index.js
+++ multicontapp/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
 
@@ -8,8 +9,37 @@ const client = redis.createClient({
     port: 6379,
 });
 
+client.set('counter', 0);
+
 app.get('/', (req, res) => {
-    res.send("Hello from my docker app");
+
+    process.exit(0); 
+
+    client.get('counter', (err, counterValue) => {
+        res.send('Counter: ' + counterValue);
+        client.set('counter', parseInt(counterValue) + 1);
+    });
+});
+
+const nwd = (a, b) => {
+    if (!b) {
+      return a;
+    }
+    return nwd(b, a % b);
+  }
+
+  app.get("/nwd/:num1/:num2", (req, res) => {
+	const num1 = req.params.num1;
+	const num2 = req.params.num2;
+	const key = `${num1}:${num2}`;
+
+	client.get(key, (err, value) => {
+		if (!value) {
+			value = nwd(num1, num2);
+		}
+		res.send(`Greatest common divisor of ${num1} and ${num2} is ${value}`);
+		client.set(key, parseInt(value));
+	});
 });
 
 app.listen(8080, () => {
